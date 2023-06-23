43ff926c649b06c15c9176ba54d361c43eb92b03
Vimaser <thfeske@gmail.com>
2023-06-22 20:15:09 -0500

    good job

--- app.js
+++ app.js
@@ -1,14 +1,101 @@
 const express = require("express");
-const volleyball = require('volleyball');
+//const volleyball = require('volleyball');
 const morgan = require('morgan');
+const postBank = require('./postBank');
+var timeAgo = require('node-time-ago');
+
+timeAgo(new Date()); 
 
 const app = express();
-app.use(volleyball);
+//app.use(volleyball);
 app.use(morgan('dev'));
+app.use(express.static('public'));
 
-app.get("/", (req, res) => res.send("Hello World!"));
+//app.get("/", (req, res) => res.send("Hello World!"));
 
-const PORT = 1337;
+
+
+app.get('/posts/:id', (req, res) => {
+  const id = req.params.id;
+  //const posts = postBank.list();
+  const post = postBank.find(id);
+  if (!post.id) {
+    // If the post wasn't found, set the HTTP status to 404 and send Not Found HTML
+    res.status(404)
+    const html = `
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>Wizard News</title>
+      <link rel="stylesheet" href="/style.css" />
+    </head>
+    <body>
+      <header><img src="/logo.png"/>Wizard News</header>
+      <div class="not-found">
+        <p>404: Page Not Found</p>
+      </div>
+    </body>
+    </html>`
+    res.send(html)
+  } else {
+    
+  {
+    res.send(`<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>
+            ${post.title}
+            <small>(by ${post.name})</small>
+            </p>
+            <span class="news-content">${post.content}</span>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>
+      </div>
+  </body>
+</html>`);}
+}})
+
+app.get('/', (req, res) => {
+   const posts = postBank.list();
+  const html = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      ${posts.map(post => `
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>
+            ${post.title}
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${timeAgo(post.date)}
+          </small>
+        </div>`
+      ).join('')}
+    </div>
+  </body>
+</html>`;
+
+  res.send(html);
+});
+
+const { PORT = 1337 } = process.env;
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
